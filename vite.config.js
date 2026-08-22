aeeb3e08783f23ba41eeb7f4e550b6478e529261
mohsinmosi322-maker <mohsinmosi322@gmail.com>
2026-08-22 10:26:44 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // Important for GitHub Pages – set to your repo name if deploying under username.github.io/repo-name
   // For username.github.io (root) use '/', for project pages use '/repo-name/'
-  base: process.env.NODE_ENV === 'production' ? '/digital-khata/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/digital-khatay/' : '/',
 })
