e347a03e568b5b21ce9e510d5478f2459c795952
mohsinmosi322-maker <mohsinmosi322@gmail.com>
2026-08-22 10:38:19 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Important for GitHub Pages – set to your repo name if deploying under username.github.io/repo-name
-  // For username.github.io (root) use '/', for project pages use '/repo-name/'
-  base: process.env.NODE_ENV === 'production' ? '/digital-khatay/' : '/',
+  base: '/Digital-khatay/',
 })
